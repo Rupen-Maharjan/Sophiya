25ed5163bf6d2a8924dcebe2e75498d793b3c95d
Rupen Maharjan <maharjanrupen7@gmail.com>
2024-08-09 20:55:24 +0545

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,112 +1,131 @@
-import Image from "next/image";
+"use client"
+
+import React, { useState } from "react";
 
 export default function Home() {
+  const [showSignup, setShowSignup] = useState(false);
+
+  const toggleForm = () => {
+    setShowSignup(!showSignup);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    // main component
+    <main className="h-screen w-full flex items-center justify-center relative">
+      <div className=" absolute top-20 lg:top-10">
+        <h1 className="text-3xl lg:text-6xl text-[#f27e3f] font-semibold">Sikandar's Clothings</h1>
+      </div>
+      {/* container */}
+      <div className="flex relative h-[50%] lg:h-[70%] w-[90%] lg:w-[60%] items-center justify-between bg-[#f1c8b2] rounded-xl overflow-hidden mt-5 lg:mt-24">
+
+        {/* slider */}
+        <div
+          className={`flex justify-center h-full w-[50%] absolute right-0 z-50 bg-cover bg-center transition-transform duration-500 ${
+            showSignup ? "-translate-x-full" : ""
+          }`}
+          style={{ backgroundImage: 'url("/images/brand.jpg")' }}
+        >
+          <div className="text-black absolute bottom-7 lg:bottom-20">
+            <button
+              className={`relative z-0 ${
+                showSignup ? "flex" : "hidden"
+              } items-center gap-2 overflow-hidden rounded-lg border-[1px] border-black px-2 py-[0.4rem] lg:px-4 lg:py-2 font-semibold uppercase text-black transition-all duration-500 before:absolute before:inset-0 before:-z-10 before:translate-x-[150%] before:translate-y-[150%] before:scale-[2.5] before:rounded-[100%] before:bg-[#f1c8b2] before:transition-transform before:duration-1000 before:content-[''] hover:scale-105 hover:text-[#a44817] hover:before:translate-x-[0%] hover:before:translate-y-[0%] active:scale-95`}
+              onClick={toggleForm}
+            >
+              Login
+            </button>
+
+            <button
+              className={`relative z-0 ${
+                showSignup ? "hidden" : "flex"
+              } items-center gap-2 overflow-hidden rounded-lg border-[1px] border-black px-2 py-[0.4rem] lg:px-4 lg:py-2 font-semibold uppercase text-black transition-all duration-500 before:absolute before:inset-0 before:-z-10 before:translate-x-[150%] before:translate-y-[150%] before:scale-[2.5] before:rounded-[100%] before:bg-[#f1c8b2] before:transition-transform before:duration-1000 before:content-[''] hover:scale-105 hover:text-[#a44817] hover:before:translate-x-[0%] hover:before:translate-y-[0%] active:scale-95`}
+              onClick={toggleForm}
+            >
+              Sign Up
+            </button>
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        {/* Wrapping div */}
+        <div
+          className={`flex justify-between h-full items-center w-[90%] mx-auto transition-transform duration-500 space-x-2`}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          {/* login form */}
+          <div className=" space-y-5">
+          <h1 className="text-3xl lg:text-6xl text-[#f27e3f] font-semibold">Login</h1>
+            <form action="" className="space-y-5">
+              <div className="bg-white py-2 px-4 lg:py-5 lg:px-7 rounded-xl lg:w-96">
+                <input
+                  autoComplete="off"
+                  className="w-full focus:outline-none text-black"
+                  placeholder="Username"
+                  type="text"
+                  name="username"
+                />
+              </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              <div className="bg-white py-2 px-4 lg:py-5 lg:px-7 rounded-xl lg:w-96">
+                <input
+                  autoComplete="off"
+                  className="w-full focus:outline-none text-black"
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                />
+              </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+              <button
+                type="submit"
+                className="relative z-0 flex items-center gap-2 overflow-hidden rounded-lg border-[1px] border-black px-2 lg:px-4 py-[0.4rem] lg:py-2 font-semibold uppercase text-black transition-all duration-500 before:absolute before:inset-0 before:-z-10 before:translate-x-[150%] before:translate-y-[150%] before:scale-[2.5] before:rounded-[100%] before:bg-[#f27e3f] before:transition-transform before:duration-1000 before:content-[''] hover:scale-105 hover:text-[#f1c8b2] hover:before:translate-x-[0%] hover:before:translate-y-[0%] active:scale-95"
+              >
+                Login
+              </button>
+            </form>
+          </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          {/* SignUp form */}
+          <div className=" space-y-5">
+            <h1 className="text-3xl lg:text-6xl text-[#f27e3f] font-semibold">Sign Up</h1>
+            <form action="" className="space-y-5">
+              <div className="bg-white py-2 px-4 lg:py-5 lg:px-7 rounded-xl lg:w-96">
+                <input
+                  autoComplete="off"
+                  className="w-full focus:outline-none text-black"
+                  placeholder="Email"
+                  type="text"
+                  name="email"
+                />
+              </div>
+
+              <div className="bg-white py-2 px-4 lg:py-5 lg:px-7 rounded-xl lg:w-96">
+                <input
+                  autoComplete="off"
+                  className="w-full focus:outline-none text-black"
+                  placeholder="Username"
+                  type="text"
+                  name="username"
+                />
+              </div>
+
+              <div className="bg-white py-2 px-4 lg:py-5 lg:px-7 rounded-xl lg:w-96">
+                <input
+                  autoComplete="off"
+                  className="w-full focus:outline-none text-black"
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="relative z-0 flex items-center gap-2 overflow-hidden rounded-lg border-[1px] border-black px-2 lg:px-4 py-[0.4rem] lg:py-2 font-semibold uppercase text-black transition-all duration-500 before:absolute before:inset-0 before:-z-10 before:translate-x-[150%] before:translate-y-[150%] before:scale-[2.5] before:rounded-[100%] before:bg-[#f27e3f] before:transition-transform before:duration-1000 before:content-[''] hover:scale-105 hover:text-[#f1c8b2] hover:before:translate-x-[0%] hover:before:translate-y-[0%] active:scale-95"
+              >
+                Sign Up
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     </main>
   );
