7fa26c0b849c9a1e2676fbe4bc5da3bb1097a686
Discort <112250385+Rupen-Maharjan@users.noreply.github.com>
2024-08-16 16:47:56 +0545

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Made You Look Clothing",
+  title: "Rupen's clothing store",
   description: "Project made by Rupen Maharjan",
 };
 
